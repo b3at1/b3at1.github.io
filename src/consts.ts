c012876705303cb0a61be644bfc78365a9550167
b3at1 <sambederman@gmail.com>
2025-06-14 23:00:02 -0400

    migrate to astro

---
+++ src/consts.ts
@@ -0,0 +1,47 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'b3at\'s blog',
+  description:
+    'AAAAAAAAAAAAAAAAAA',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'b3at',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 4,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/about',
+    label: 'about',
+  },
+  {
+    href: '/blog',
+    label: 'blog',
+  }
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/b3at1',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://www.linkedin.com/in/sam-bederman/',
+    label: 'LinkedIn',
+  },
+  {
+    href: 'mailto:sambederman@gmail.com',
+    label: 'Email',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
---
+++ src/content/authors/b3at.md
@@ -0,0 +1,9 @@
+---
+name: 'b3at'
+pronouns: 'he/him'
+avatar: 'https://avatars.githubusercontent.com/u/94728927'
+bio: 'BLIPNAR GINGLEBAN DROXNOY!!!'
+website: 'https://b3at1.github.io'
+github: 'https://github.com/b3at1'
+mail: 'sambederman@gmail.com'
+---
